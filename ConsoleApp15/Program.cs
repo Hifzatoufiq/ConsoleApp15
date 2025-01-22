7d099de9fa0c9fb8cefe37998ff69dee92257048
hifza <hifzatoufiq@gmail.com>
2025-01-22 09:59:14 +0500

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+
+    //parent class
+    class fruit
+    {
+        protected void apple()
+        {
+            Console.WriteLine("apple is a fruit");
+        }
+    }
+    //child1 extend parent
+    class fruit1 : fruit
+    {
+        public void orange()
+        {
+
+            Console.WriteLine("orange is fruits");
+        }
+        //child2 extend child1 child1 have a data in parent
+        class fruit2 : fruit1
+        {
+            public void grapes()
+            {
+
+                Console.WriteLine("grapes is fruits");
+            }
+            internal class Program
+            {
+                static void Main(string[] args)
+                {
+                    fruit2 obj=new fruit2();
+                    obj.grapes();
+                    obj.orange();
+                    obj.apple();
+
+                }
+            }
+        }
+    }
+
+}
